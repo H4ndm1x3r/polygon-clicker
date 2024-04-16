96d8270f157988552a39d7c912311db4aa2f259a
NKrasuski <nlloyd0001@gmail.com>
2024-04-16 18:08:33 +0200

    Add clicker page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { ClickerPageComponent } from './components';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: ClickerPageComponent },
+];
---
+++ src/app/components/pages/clicker/clicker-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clicker-page',
+  templateUrl: 'clicker-page.component.html',
+  standalone: true,
+})
+export class ClickerPageComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
