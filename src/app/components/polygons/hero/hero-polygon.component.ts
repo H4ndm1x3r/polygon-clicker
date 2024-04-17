01c0b39d53a7cd85a6c58955b290c56a06ac30b5
NKrasuski <nlloyd0001@gmail.com>
2024-04-17 19:38:48 +0200

    Make hero polygon accessible

--- src/app/components/polygons/hero/hero-polygon.component.ts
+++ src/app/components/polygons/hero/hero-polygon.component.ts
@@ -4,18 +4,16 @@ import { HeroPolygonService } from './hero-polygon.service';
 @Component({
   selector: 'clicker-hero-polygon',
   standalone: true,
-  // providers: [HeroPolygonService],
   templateUrl: 'hero-polygon.component.html',
   styleUrl: 'hero-polygon.component.scss'
 })
 export class HeroPolygonComponent {
   private readonly heroPolygonService = inject(HeroPolygonService);
 
-  @HostListener('click') public async onClick(): Promise<void> {
+  @HostListener('click')
+  @HostListener('keyup.space')
+  @HostListener('keyup.enter')
+  public async onClick(): Promise<void> {
     this.heroPolygonService.incrementClick();
   }
-
-  constructor() {}
-
-
 }
